ee80f9e3160dbc6ee72cf7ea5413f825c4b591af
syedawaisalishah <48494631+syedawaisalishah@users.noreply.github.com>
2025-06-14 19:25:46 +0000

    d

--- tut1.js
+++ tut1.js
@@ -2,9 +2,22 @@
 /*
 so that why let use
 */
+"use strict";
 let name='Awais';
 var password="3";
 const PI=3.4;
 rollno=2;
 console.log(name);
 console.table([name,password,PI,rollno]);
+console.debug('d');
+console.info();
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
