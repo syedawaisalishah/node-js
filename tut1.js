cd90e52e116fc55b45b45142d2ae1b073ff46899
syedawaisalishah <48494631+syedawaisalishah@users.noreply.github.com>
2025-06-14 19:03:46 +0000

    add let const var

---
+++ tut1.js
@@ -0,0 +1,10 @@
+// var use but var have a problem where it was access out of scop and it change when same name variable change
+/*
+so that why let use
+*/
+let name='Awais';
+var password="3";
+const PI=3.4;
+rollno=2;
+console.log(name);
+console.table([name,password,PI,rollno]);
